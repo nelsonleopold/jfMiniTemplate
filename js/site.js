b03e29217b107358805099080914630a9e2a4d9e
nelsonleopold <nelsonleopold@gmail.com>
2022-04-08 12:27:15 -0700

    created small to do app for app page

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 // Step One, get the information (controller, accept requests)
 function getMessage() {
-    let msg = document.getElementById("message").value;
+    let msg = document.getElementById("txtMessage").value;
 
     displayMessage(msg);
 }
@@ -10,8 +10,21 @@ function getMessage() {
 // Final Step, display the information (view, displays stuff on screen)
 function displayMessage(message) {
 
-    let msg = `Hello nelson the dev ${message}`;
 
+    // Bootstrap numbered list format: <li class="list-group-item">A list item</li>
+    // let item = `<li class="list-group-item">${message}</li>`;
+
+    // first get the ol element from the page
     element = document.getElementById("results");
-    element.innerHTML = msg;
+
+    // next create a new li element
+    let item = document.createElement("li");
+
+    // add classes to li element
+    item.classList.add("list-group-item");
+
+    // set the message for the li element
+    item.innerHTML = message;
+
+    element.appendChild(item);
 }
